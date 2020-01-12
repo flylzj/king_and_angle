b45ddeaa523eed4332d5fbec421f1088b924c040
flylzj <1449902124@qq.com>
2020-01-12 09:49:02 +0800

    Update config.go

--- src/config/config.go
+++ src/config/config.go
@@ -17,7 +17,7 @@ func init(){
 	if err != nil{
 		log.Fatalln("打开文件失败", err.Error())
 	}
-	MYSQLURI = "christmas_user:lzjlzj123@(gz-cdb-h25tz5ek.sql.tencentcdb.com:62581)/christmas?charset=utf8&parseTime=True&loc=Local"
+	MYSQLURI = "root:123456@(localhost)/christmas?charset=utf8&parseTime=True&loc=Local"
 	Info = log.New(os.Stdout, "Info:", log.Ldate | log.Ltime | log.Lshortfile)
 	Error = log.New(io.MultiWriter(os.Stdout, errFile), "Error:", log.Ldate | log.Ltime | log.Llongfile)
 }
